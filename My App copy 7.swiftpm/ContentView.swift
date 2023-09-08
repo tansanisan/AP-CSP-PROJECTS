cc3dab4cc820c6aa044688fd4fb82ff4a3e6d3b8
tansanisan <144126216+tansanisan@users.noreply.github.com>
2023-09-08 20:20:42 +0300

    Add files via upload

---
+++ My App copy 7.swiftpm/ContentView.swift
@@ -0,0 +1,168 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            //Color.white
+            Path() { path in 
+                
+                path.move(to: CGPoint(x: 68.7746827672824, y: 135.6085612899174))
+                path.addLine(to: CGPoint(x: 85.7293902332723, y: 46.9681608536889))
+                
+                path.move(to: CGPoint(x: 105.2021025321361, y: 175.5441971964526))
+                path.addLine(to: CGPoint(x: 158.7025263741651, y: 123.0698088801611))
+                path.move(to: CGPoint(x: 102.7103019696306, y: 173.0523966339472))
+                path.addLine(to: CGPoint(x: 156.3573023153365, y: 120.8711613250092))
+                path.move(to: CGPoint(x: 100, y: 170))
+                path.addLine(to: CGPoint(x: 153.4257722418006, y: 117.6464782441198))
+                path.move(to: CGPoint( x:65.2119242534289 , y :134.7654011187432))
+                path.addLine(to : CGPoint(x:82.2707223019063 , y:45.7324962355937))
+                path.move(to: CGPoint(x: 32.4964211467599, y: 121.9128820411232))
+                path.addLine(to: CGPoint(x: 67.5043895282446, y: 117.3675456487812))
+                path.move(to: CGPoint(x:32.0883618471039 , y:118.4742965138168))
+                path.addLine(to: CGPoint(x:68.4530331268466 , y:113.8891857872406))
+                path.move(to: CGPoint(x:30.9724790285597, y: 115.5009768034643))
+                path.addLine(to: CGPoint(x:70 , y: 110))   
+                path.move(to: CGPoint(x:30.7930407285533, y: 111.9122108033362))
+                path.addLine(to: CGPoint(x:70.0900284299565 , y: 107.0673767031632))
+                path.move(to: CGPoint(x:30, y: 110))
+                path.addLine(to: CGPoint(x:71.1666582299949 , y: 104.0169256030542)) 
+                path.move(to: CGPoint(x:30.2547258285341, y: 106.1701852031311))
+                path.addLine(to: CGPoint(x:71.8844114300205 , y: 100.6075979029325)) 
+                path.move(to: CGPoint(x:30, y: 100))
+                path.addLine(to: CGPoint(x:63.0919347297066 , y: 95.4038872027467)) 
+                path.move(to: CGPoint(x:21.4622491282201, y: 91.4562446026057))
+                path.addLine(to: CGPoint(x:56.4527176294695 , y: 56.4657761013562)) 
+                path.move(to: CGPoint(x:24.2371242776009, y: 93.9289141533168))
+                path.addLine(to: CGPoint(x:58.9821777323789 , y: 58.8041333383773)) 
+                path.move(to: CGPoint(x:26.3889126518495, y: 96.2072783142859))
+                path.addLine(to: CGPoint(x:61.4504055734287 , y: 60.9559217126258)) 
+                path.move(to: CGPoint(x:28.540701026098, y: 98.1692030084537))
+                path.addLine(to: CGPoint(x:63.7287697343978 , y: 63.2975737669551))
+                path.move(to: CGPoint(x:32.9075656679554, y: 98.4223545818947))
+                path.addLine(to: CGPoint(x:65.8172702152861 , y: 65.5759379279242))  
+                path.move(to: CGPoint(x:38.350324496937, y: 97.9793393283729))
+                path.addLine(to: CGPoint(x:68.0323464828949 , y: 67.537862622092))
+                path.move(to: CGPoint(x:48.5966867525519, y: 96.5725349826743))
+                path.addLine(to: CGPoint(x:72.6877654469299 , y: 72.3748585949582))
+                path.move(to: CGPoint(x:43.6665075391981, y: 96.9034451412486))
+                path.addLine(to: CGPoint(x:70 , y: 70)) 
+                path.move(to: CGPoint(x:66.3652456142309, y: 97.8967404952965))
+                path.addLine(to: CGPoint(x:73.1788349987015 , y: 94.124932086036))
+                path.move(to: CGPoint(x:61.1333823368696, y: 92.6648772179352))
+                path.addLine(to: CGPoint(x:74.517218627794 , y: 86.4596440285066))
+                path.move(to: CGPoint(x:62.8367796829873, y: 89.2580825256999))
+                path.addLine(to: CGPoint(x:74.7605611058108 , y: 83.1745205752797)) 
+                path.move(to: CGPoint(x:67.9469717213402, y: 83.0528493362713))
+                path.addLine(to: CGPoint(x:76.0989447349032 , y: 74.7792050836999)) 
+                path.move(to: CGPoint(x:109.6528767386132, y: 102.4987441741016))
+                path.addLine(to: CGPoint(x:127.7413964340311 , y: 49.0553905285486))
+                path.move(to: CGPoint(x:106.6929371520902, y: 101.3476565571205))
+                path.addLine(to: CGPoint(x:124.2881335830877 , y: 48.89094944040846)) 
+                path.move(to: CGPoint(x:1103.7538953941446, y: 100.3390155835116))
+                path.addLine(to: CGPoint(x:121.2735167663455 , y: 46.9874084183928)) 
+                path.move(to: CGPoint(x:100.6621975049326, y: 99.7048211446989))
+                path.addLine(to: CGPoint(x:118.3403674868367 , y: 46.4324882844317))  
+                path.move(to: CGPoint(x:124.1131074665568, y: 105.1068079528562))
+                path.addLine(to: CGPoint(x:87.5708985752148, y: 34.1407790914093))
+                path.move(to: CGPoint(x:126.8934929256806, y: 103.782814877083))
+                path.addLine(to: CGPoint(x:90.8808812646479, y: 32.2871887853267))
+                path.move(to: CGPoint(x:129.2766804620725, y: 101.9292245710004))
+                path.addLine(to: CGPoint(x:93.6612667237718, y: 31.2279943247081))
+                path.move(to: CGPoint(x:132.4542638439283, y: 101.0024294179591))
+                path.addLine(to: CGPoint(x:96.574051490473, y: 30.3011991716668))
+                path.move(to: CGPoint(x:135.1022499954748, y: 99.6784363421859))
+                path.addLine(to: CGPoint(x:99.3544369495968, y: 28.3152095580069))
+                path.move(to: CGPoint(x:122.9074545824819, y: 119.6692294381109))
+                path.addLine(to: CGPoint(x:157.2427985592122, y: 85.5511977650305))
+                path.move(to: CGPoint(x:124.9817817393289, y: 122.0488975329357))
+                path.addLine(to: CGPoint(x:159.2482268028937, y: 87.9440866441989)) 
+                path.move(to: CGPoint(x:127.1638430995087, y: 124.4734101553577))
+                path.addLine(to: CGPoint(x:161.1878369008314 , y: 90.2877821792068))
+                path.move(to: CGPoint(x:129.6691728093448, y: 126.5746544281235))
+                path.addLine(to: CGPoint(x:163.6931666106675 , y: 92.1465751897304)) 
+                path.move(to: CGPoint(x:131.7704170821106, y: 129.0799841379597))
+                path.addLine(to: CGPoint(x:165.9560450582614 , y: 94.5710878121525)) 
+                path.move(to: CGPoint(x:134.0147891988053, y: 131.3527542618895))
+                path.addLine(to: CGPoint(x:168.3043920601413 , y: 96.7269788234816))
+                path.move(to: CGPoint(x:83.2129802491216, y: 152.6673620338807))
+                path.addLine(to: CGPoint(x:96.0016332239426, y: 83.6717898116983))
+                path.move(to: CGPoint(x:86.3261436278547, y: 153.4340985271098))
+                path.addLine(to: CGPoint(x:100.3085089516072, y: 81.4474693119197))  
+                path.move(to: CGPoint(x:89.2128255011456, y: 154.2459778039728))
+                path.addLine(to: CGPoint(x:104.0070701017611, y: 79.4628755240322))
+                path.move(to: CGPoint(x:92.1897161829767, y: 154.6970218466745))
+                path.addLine(to: CGPoint(x:100.8497618028493, y: 112.9303434924978)) 
+                path.move(to: CGPoint(x:95, y: 155))
+                path.addLine(to: CGPoint(x:104.1874877188418, y: 113.1107611095785)) 
+                path.move(to: CGPoint(x:98.3239151637198, y: 156.0501539747796))
+                path.addLine(to: CGPoint(x:107.164378400673, y: 112.2086730241751))
+                path.move(to: CGPoint(x:101.6616410797123, y: 156.8620332516426))
+                path.addLine(to: CGPoint(x:110.5923131252058, y: 111.9380465985541))  
+                path.move(to: CGPoint(x:104.8189493786242, y: 157.8543301455864))
+                path.addLine(to: CGPoint(x:117.1775561486506, y: 111.3967937473121))
+                path.move(to: CGPoint(x:107.976257677536, y: 157.4032861028847))
+                path.addLine(to: CGPoint(x:113.9300390411984, y: 111.4870025558524))  
+                path.move(to: CGPoint(x:111.1619739510576, y: 157.9206392803718))
+                path.addLine(to: CGPoint(x:120.0288634244172, y: 111.3439899777816))   
+                path.move(to: CGPoint(x:73.8598872010619, y: 116.134148658792))
+                path.addLine(to: CGPoint(x:133.2782384995523, y: 108.2864418835197))  
+                path.move(to: CGPoint(x:74.0637237406794, y: 113.07660056453))
+                path.addLine(to: CGPoint(x:136.9472962126667, y: 104.2097110911704))  
+                path.move(to: CGPoint(x:74.7771516293405, y: 109.8152159306506))
+                path.addLine(to: CGPoint(x:140, y: 100)) 
+                path.move(to: CGPoint(x:76.3059256764715, y: 106.5538312967712))
+                path.addLine(to: CGPoint(x:143.5719837502342, y: 96.5658408555156))                
+                path.move(to: CGPoint(x:76.2040074066627, y: 102.6809370440394))
+                path.addLine(to: CGPoint(x:147.1391231935398, y: 93.4063744914449))                     
+                path.move(to: CGPoint(x:76.8155170255151, y: 99.6233889497775))
+                path.addLine(to: CGPoint(x:150, y: 90)) 
+                path.move(to: CGPoint(x:78.1404545330286, y: 96.2600860460894))
+                path.addLine(to: CGPoint(x:152.74462803302, y: 86.2720956048337))                 
+                path.move(to: CGPoint(x:78.751964151881, y: 92.7948648725925))
+                path.addLine(to: CGPoint(x:153.152301112255, y: 82.6030378917194))            
+                path.move(to: CGPoint(x:79.5396255191129, y: 86.7796546355151))
+                path.addLine(to: CGPoint(x:133.3909294140763, y: 60.4981570408439))  
+                path.move(to: CGPoint(x:80.4414416130477, y: 83.0435593892138))
+                path.addLine(to: CGPoint(x:131.4584663556446, y: 57.9215396296017))  
+                path.move(to: CGPoint(x:81.4720885775446, y: 79.3074641429125))
+                path.addLine(to: CGPoint(x:130.0413267794614, y: 55.4737530889215))  
+                             
+                 path.move(to: CGPoint(x:82.1162429303551, y: 75.1848762849249))
+                 path.addLine(to: CGPoint(x:128.2376945915918, y: 52.5106430659929))  
+                             
+                 path.move(to: CGPoint(x:82.2450738009172, y: 71.964104520872))
+                 path.addLine(to: CGPoint(x:122.3114745457346, y: 51.9953195837445))  
+                             //                    
+                //                    
+                //                    
+                //                    
+                //                    
+                                    
+                                
+            }
+            .stroke(Color.blue, lineWidth: 1)
+        }
+   }           
+             
+               
+                      
+
+
+                 
+              
+
+                
+//                }
+//                .stroke(Color.blue, lineWidth: 1)
+//            }}}
+//            
+//            
+//            
+         
+     
+         
+            
+        
+    
+}
