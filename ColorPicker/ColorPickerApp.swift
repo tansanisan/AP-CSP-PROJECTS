85f9cfd4b134f15a4713323855e90831c9e3c97d
tansanisan <144126216+tansanisan@users.noreply.github.com>
2023-09-12 21:32:32 +0300

    Add files via upload

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Tansa Nisan Gunerhan on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
