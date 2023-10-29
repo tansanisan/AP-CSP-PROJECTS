a2e6475f4e16e924055f609361981672c2de860e
tansanisan <144126216+tansanisan@users.noreply.github.com>
2023-10-29 10:08:38 +0300

    all logic gate content views

--- ContentView.swift
+++ ContentView.swift
@@ -1,57 +1,32 @@
 import SwiftUI
 
+
 struct ContentView: View {
-        @State private var numConvert: Int = 0
-    // burada direk variable tanimliyorum ve islem yaptirdim
-        var quotient: Int {
-            return numConvert / 2
-        }
-    
-        var remainder: Int {
-            return numConvert % 2
-        }
-    
-        var body: some View {
-            VStack {
-                Text("Number base converter")
-                    .padding()
-    // tekrardan kullanici input aliyorum
-                TextField("Enter integer", value: $numConvert, formatter: NumberFormatter())
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-    // functionum yaptigi olay buraya display ediyor
-                Text(numToBinary())
+    @State private var input1 = false
+    @State private var input2 = false
+    @State private var result = true
+
+    //variable tanimladim
+    var body: some View {
+        VStack {
+            Text("NOR Logic Gate Tester")
+                .font(.largeTitle)
+
+            Toggle("Input 1", isOn: $input1)
+                .padding()
+
+            Toggle("Input 2", isOn: $input2)
+                .padding()
+
+            Button("Test NOR Gate") {
+                // burada or ile cikan sonucun ile result farkli ise true olacak
+                result = !(input1 || input2)
             }
             .padding()
-        }
-    
-    // burada sayiyi binart yapma islemini yaptim, result bos biraktim, cunku ya bir ya sifir olacak bir sey yapiyoruz
-        func numToBinary() -> String {
-            var result = ""
-            var num = numConvert
-    
-            while num != 0 {
-                if num % 2 == 0 {
-                    result = "0" + result
-                } else {
-                    result = "1" + result
-                }
-                num /= 2
-            }
-            return result.isEmpty ? "0" : result
-            // burada swift 100 days ogrendigim seyi yaptim , uzun if kullanmak yerine eger result bos ise sifir, degilse result return et dedim.
-        }
-    
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
-    
-    
-    //Binary to number converter page
 
-        
-
+            Text("Result: \(result ? "True" : "False")")
+                .font(.title)
+        }
+        //display edilecek textim
+    }
+}
